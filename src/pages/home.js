ba23cab3637b19df329e9e5255c143e7098cedb8
Adriel <adriel.2000@hotmail.com>
2020-02-11 10:30:12 -0300

    Adicionando className no card.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {Card, Button} from 'react-bootstrap';
 import {Card, Button, CardColumns} from 'react-bootstrap';
 
 export default class Home extends React.Component{
 
   render(){
     return(
-        <div class="col-12 card-deck">
-          <Card style={{ width: '18rem', marginRight: '1rem' }}>
       <div className="container-fluid body-main">
         <CardColumns>
+          <Card className="anime">
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
@@ -19,7 +17,7 @@ export default class Home extends React.Component{
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '18rem', marginRight: '1rem' }}>
+          <Card className="anime">
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
@@ -29,7 +27,7 @@ export default class Home extends React.Component{
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '18rem', marginRight: '1rem' }}>
+          <Card className="anime">
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
@@ -39,7 +37,98 @@ export default class Home extends React.Component{
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
-        </div>);
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card className="anime">
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          
         </CardColumns>
       </div>);
   }
